a0fc7da7055505b7f56672568f0014214ca399f3
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-03 09:51:48 -0700

    Add add_interest method to patron class and test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -5,9 +5,27 @@ RSpec.describe Patron do
     subject(:patron_1) { described_class.new("Bob", 20) }
 
     
-    describe "#initialize" do
-        it "exists" do 
+    describe '#initialize' do
+        it 'exists' do 
             expect(patron_1).to be_an_instance_of(Patron)
+        end
+
+        it 'has attributes' do
+            expect(patron_1.name).to eq("Bob")
+            expect(patron_1.spending_money).to eq(20)
+        end
+
+        it 'has a list of interests' do
+            expect(patron_1.interests).to eq([])
+        end
+    end
+
+    describe '#add_interest' do
+        it 'adds an interest to the patrons interests' do
+            patron_1.add_interest("Dead Sea Scrolls")
+            patron_1.add_interest("Gems and Minerals")
+
+            expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
         end
     end
 end
