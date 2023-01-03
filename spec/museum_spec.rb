d2d5ea8ccb70b6c9c5859c13678617b8da73e8f4
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-03 10:07:57 -0700

    Add info to museum class and attribute test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -10,5 +10,10 @@ RSpec.describe Museum do
         it 'exists' do 
             expect(dmns).to be_an_instance_of(Museum)
         end
+
+        it 'has attributes' do
+            expect(dmns.name).to eq("Denver Museum of Nature and Science")
+            expect(dmns.exhibits).to eq([])
+        end
     end
 end
