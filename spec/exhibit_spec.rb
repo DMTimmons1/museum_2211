7746eb131b5cff9d135ed726bb4206ee19861135
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-03 09:24:30 -0700

    Add attribute test and info to exhibit class

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe Exhibit do
         expect(exhibit).to be_an_instance_of(Exhibit)
     end
 
+    it 'has attributes' do
+        expect(exhibit.name).to eq("Gems and Minerals")
+        expect(exhibit.cost).to eq(0)
+    end
   end
 end
